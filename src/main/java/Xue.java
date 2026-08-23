d6d50f7b78a3646350b2645970a35d0be06ef386
xinyu0410 <xxuexinyu@gmail.com>
2026-08-23 16:01:35 +0800

    Add interactive greeting and farewell output

--- src/main/java/Xue.java
+++ src/main/java/Xue.java
@@ -1,10 +1,18 @@
 public class Xue {
     public static void main(String[] args) {
+        String separator = "________________________________________________________________________________";
         String banner = "██   ██  ██   ██  ███████\n"
                 + " ██ ██   ██   ██  ██\n"
                 + "  ███    ██   ██  █████\n"
                 + " ██ ██   ██   ██  ██\n"
-                + "██   ██   █████   ███████\n";
+                + "██   ██   █████   ███████";
+
+        System.out.println(separator);
         System.out.println(banner);
+        System.out.println("Hello! I'm Xue.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
